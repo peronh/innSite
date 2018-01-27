d6ba1828de7b430d28c039758b5856afe4ae8adc
pronyecz <pronyecz@gmail.com>
2018-01-27 23:58:30 +0100

    update desktop js

--- hufo.js
+++ hufo.js
@@ -1,2 +1,2 @@
 $(document).ready(function(){$(".info").on("click",function(){$("#mySidenav").css({"width":"85vw","transition":"width 200ms","-webkit-transition":"width 300ms"})});$(".closebtn").on("click",function(){$("#mySidenav").css({"width":"0","transition":"width 200ms","-webkit-transition":"width 300ms"})});$(document).on("scroll",function(){$('.btntop').on("click",function(){$(document).scrollTop(0)});if($(document).scrollTop()>200){$(".btntop").show(250)}
-else{$(".btntop").hide(250)}});var targetWidth=1500;$('.desktop').on('click',function(){$('meta[name="viewport"]').attr('content','width='+targetWidth)})})
+else{$(".btntop").hide(250)}});var targetWidth=1400;$('.desktop').on('click',function(){$('meta[name="viewport"]').attr('content','width='+targetWidth)})})
